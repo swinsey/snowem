2a9bf2b838e36ffdc180172f816a87db8222be54
root <root@mail1.peercall.vn>
2017-07-14 07:00:57 -0700

    update display of demo page

--- html/js/app.js
+++ html/js/app.js
@@ -27,6 +27,8 @@ window.snowAsyncInit = function() {
    SnowSDK.init(function (){
       var isPublisher = 0;
       var channelid = 0;
+      var channelid1 = 0;
+      var channelid2 = 0;
       var peer = null;
       var config = {
          'servername': "media.peercall.vn",
@@ -40,39 +42,76 @@ window.snowAsyncInit = function() {
             'channelid': channelid,
             'localVideoId': document.getElementById('localVideo'),
             'remoteVideoId': null
-            //'remoteVideoId': document.getElementById('remoteVideo')
          };
 
          peer.publish(settings);
       }
 
       //API calls go here.
-      document.getElementById("publishBtn").addEventListener("click", function() {
+      $("#publishBtn").click(function() {
          isPublisher = 1;
+         $("#floatDiv").hide();
+         $("#publishVideoDiv").attr('class','col-md-6');
+         $("#publishDiv").append('<div class="col-md-3 text-left"> Your webcam\'s channel id: <span id="yourId"></span></div>');
          peer = SnowSDK.createPeer(config);
          peer.createChannel({name: "demo"},onPublishChannelCreated);
       });
 
       function onPlayChannelCreated(peer) {
          console.log("onCreate: peer=" + JSON.stringify(peer));
-         document.getElementById("yourId").innerHTML = peer.peerId;
          var settings = {
             'channelid': channelid,
-            //'localVideoId': document.getElementById('playLocalVideo'),
             'localVideoId': null,
             'remoteVideoId': document.getElementById('playRemoteVideo')
          };
 
          peer.play(settings);
       }
-
-
-      document.getElementById("subscribeBtn").addEventListener("click", function() {
-         channelid = parseInt(document.getElementById("subscribeChannelId").value);
+      $("#playBtn").click(function() {
+         channelid = parseInt(document.getElementById("playChannelId").value);
          isPublisher = 0;
+         $("#playBtnDiv").hide();
          peer = SnowSDK.createPeer(config);
          peer.createChannel({name: "demo"},onPlayChannelCreated);
       });
+
+      function onPlayChannelCreated1(peer) {
+         console.log("onCreate: peer=" + channelid1);
+         var settings = {
+            'channelid': channelid1,
+            'localVideoId': null,
+            'remoteVideoId': document.getElementById('playRemoteVideo1')
+         };
+
+         peer.play(settings);
+      }
+      $("#playBtn1").click(function() {
+         channelid1 = parseInt(document.getElementById("playChannelId1").value);
+         isPublisher = 0;
+         $("#playBtnDiv1").hide();
+         peer = SnowSDK.createPeer(config);
+         peer.createChannel({name: "demo"},onPlayChannelCreated1);
+      });
+
+      function onPlayChannelCreated2(peer) {
+         console.log("onCreate: peer=" + JSON.stringify(peer));
+         var settings = {
+            'channelid': channelid2,
+            'localVideoId': null,
+            'remoteVideoId': document.getElementById('playRemoteVideo2')
+         };
+
+         peer.play(settings);
+      }
+      $("#playBtn2").click(function() {
+         channelid2 = parseInt(document.getElementById("playChannelId2").value);
+         isPublisher = 0;
+         $("#playBtnDiv2").hide();
+         peer = SnowSDK.createPeer(config);
+         peer.createChannel({name: "demo"},onPlayChannelCreated2);
+      });
+
+
    })
 }
 
